996826f85225792ea3c77942f0aef2b338700ba9
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-02 21:26:06 +0900

    회원가입 useAlert, react-hook-form 사용

---
+++ src/hooks/useAlert.ts
@@ -0,0 +1,7 @@
+import { useCallback } from "react"
+
+export const useAlert = () => {
+    const showAlert = useCallback((message: string) => {
+        window.alert(message);
+    }, [])
+}
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -4,28 +4,48 @@ import Title from '../components/common/Title';
 import InputText from '../components/common/InputText';
 import Button from '../components/common/Button';
 import { Link } from 'react-router-dom';
+import {useForm} from "react-hook-form";
+import { signup } from '../api/auth.api';
+import { useNavigate } from 'react-router-dom';
+import { useAlert } from '../hooks/useAlert';
+
+export interface SignupProps {
+    email: string;
+    password: string;
+}
 
 const Signup = () => {
+    const navigate = useNavigate();
+    const showAlert = useAlert();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
 
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(email, password)
+    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    //     event.preventDefault();
+    //     console.log(email, password)
+    // }
+    const {register, handleSubmit, formState: {errors},} = useForm<SignupProps>();
+
+    const onsubmit = (data: SignupProps) => {
+        signup(data).then((res) => {
+            showAlert('회원가입이 완료되었습니다.');
+            navigate("/login");
+        })
     }
-
     return (
         <>
             <Title size="large">회원가입</Title>
             <SignupStyle>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(onsubmit)}>
                     <fieldset>
-                        <InputText placeholder='이메일' inputType='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <InputText placeholder='이메일' inputType='email' {...register("email", {required: true})} />
+                        {errors.email && <p className='error-text'>이메일을 입력해주세요.</p> }
                     </fieldset>
                     <fieldset>
-                        <InputText placeholder='비밀번호' inputType='password' value={email} onChange={(e) => setPassword(e.target.value)}/>
+                        <InputText placeholder='비밀번호' inputType='password' {...register("password", {required: true})}/>
+                        {errors.password && <p className='error-text'>비밀번호를 입력해주세요.</p> }
                     </fieldset>
                     <fieldset>
                         <Button size="medium" type="submit"scheme="primary">

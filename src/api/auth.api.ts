ea909eed5d2be154e659a423cffbda40e03fb741
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-03 10:31:16 +0900

    비밀번호 초기화 페이지 구현 및 기능 구현

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -6,3 +6,14 @@ export const signup = async(userData: SignupProps) => {
 ;
 return response.data;
 }
+
+export const resetRequest = async(data: SignupProps) => {
+    const response = await httpClient.post("/users/reset", data);
+    return response.data;
+}
+
+
+export const resetpassword = async(data: SignupProps) => {
+    const response = await httpClient.put("/users/reset", data);
+    return response.data;
+}
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Title from '../components/common/Title';
@@ -8,48 +9,55 @@ import {useForm} from "react-hook-form";
 import { signup } from '../api/auth.api';
 import { useNavigate } from 'react-router-dom';
 import { useAlert } from '../hooks/useAlert';
-
+import { SignupStyle } from './Signup';
+import { resetPassword } from '../api/auth.api';
+import { resetRequest } from '../api/auth.api';
 export interface SignupProps {
     email: string;
     password: string;
 }
 
-const Signup = () => {
+const ResetPassword = () => {
     const navigate = useNavigate();
     const showAlert = useAlert();
+    const [resetRequested, setResetRequested] = useState(false)
 
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
-
-
-    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault();
-    //     console.log(email, password)
-    // }
     const {register, handleSubmit, formState: {errors},} = useForm<SignupProps>();
 
     const onsubmit = (data: SignupProps) => {
-        signup(data).then((res) => {
-            showAlert('회원가입이 완료되었습니다.');
-            navigate("/login");
-        })
+        if (resetRequested) {
+            // 초기화
+            resetPassword(data).then(() => {
+                showAlert("비밀번호가 초기화되었습니다.");
+                navigate("/login")
+            })
+        } else {
+            // 요청
+            resetRequest(data).then(() => {
+                setResetRequested(true);
+            })
+        }
     }
     return (
         <>
-            <Title size="large">회원가입</Title>
+            <Title size="large">비밀번호 초기화</Title>
             <SignupStyle>
                 <form onSubmit={handleSubmit(onsubmit)}>
                     <fieldset>
                         <InputText placeholder='이메일' inputType='email' {...register("email", {required: true})} />
                         {errors.email && <p className='error-text'>이메일을 입력해주세요.</p> }
                     </fieldset>
-                    <fieldset>
-                        <InputText placeholder='비밀번호' inputType='password' {...register("password", {required: true})}/>
-                        {errors.password && <p className='error-text'>비밀번호를 입력해주세요.</p> }
-                    </fieldset>
+
+                    {resetRequested && (
+                        <fieldset>
+                            <InputText placeholder='비밀번호' inputType='password' {...register("password", {required: true})}/>
+                            {errors.password && <p className='error-text'>비밀번호를 입력해주세요.</p> }
+                        </fieldset>
+                    )}
+                    
                     <fieldset>
                         <Button size="medium" type="submit"scheme="primary">
-                            회원가입
+                            {resetRequested ? "비밀번호 초기화" : "초기화 요청"}
                         </Button>
                     </fieldset>
                     <div className='info'>
@@ -61,24 +69,6 @@ const Signup = () => {
     );
 };
 
-export const SignupStyle = styled.div`
-    fieldset{
-        border:0;
-        padding: 0 0 8px 0;
-        .error-text{
-            color: red;
-        }
-    }
-    input{
-        width: 100%;
-    }
-    button{
-        width: 100%;
-    }
-    .info{
-        text-align: center;
-        padding: 16px 0 0 0;
-    }
-`;
 
-export default Signup;
+
+export default ResetPassword;

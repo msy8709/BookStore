05430ee2f2d8864b32ac12c850ec9ce9e63bf1d1
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-03 14:51:04 +0900

    book filter에서 usesearchparams 사용

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react";
 import { fetchCategory } from "../api/category.api";
 import { Category } from "../models/category.model";
+import { useLocation } from "react-router-dom";
 
 export const useCategory = () => {
+    const location = useLocation();
     const[category,setCategory] = useState<Category[]>([]);
 
+
+    const setActive = () => {
+        const params = new URLSearchParams(location.search);
+        if(params.get("category_id")){
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: item.id === Number(params.get('category_id')),
+                    }
+                }) 
+            })
+        }else{
+            setCategory((prev) => {
+                return prev.map((item) => {
+                    return {
+                        ...item,
+                        isActive: false,
+                    }
+                }) 
+            })
+        }
+    }
     useEffect(() => {
         fetchCategory().then((category) => {
             if (!category) return;
-            
+
             const categoryWithAll = [
                 {
                     id: null,
@@ -18,7 +43,14 @@ export const useCategory = () => {
             ];
 
             setCategory(categoryWithAll);
+            setActive()
         })
     }, [])
+
+    useEffect(() => {
+        setActive();
+    } ,[location.search]);
+
+
     return {category};
 }

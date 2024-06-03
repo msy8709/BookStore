98d248f5f2268f7a8855f086d8659f5896bec17f
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-03 14:21:55 +0900

    book item style 추가

--- src/components/books/BookItem.tsx
+++ src/components/books/BookItem.tsx
@@ -13,7 +13,7 @@ const BookItem = ({book} : Props) => {
         <div>
             <BookItemStyle>
                 <div className='img'>
-                    <img src={getImgSrc(book.id)} alt={book.title} />
+                    <img src={getImgSrc(book.img)} alt={book.title} />
                 </div>
                 <div className='content'>
                     <h2 className='title'>{book.title}</h2>
@@ -30,5 +30,62 @@ const BookItem = ({book} : Props) => {
     );
 };
 
-const BookItemStyle = styled.div``;
+const BookItemStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 0 4px rgba(0,0,0,0.2);
+    
+    .img{
+        border-radius: ${({theme}) => theme.borderRadius.default};
+        overflow: hidden;
+        img{
+            max-width: 100%;
+        }
+    }
+    .content{
+        padding: 16px;
+        position: relative;
+        .title{
+            font-size: 1.25rem;
+            font-weight: 700;
+            margin: 0 0 12px 0;
+        }
+        .summary{
+            font-size: 0.875rem;
+            color:${({theme}) => theme.color.secondary};
+            margin: 0 0 4px 0;
+        }
+        .author{
+            font-size: 0.875rem;
+            color:${({theme}) => theme.color.secondary};
+            margin: 0 0 4px 0;
+        }
+        .price{
+            font-size: 1rem;
+            color:${({theme}) => theme.color.secondary};
+            margin: 0 0 4px 0;
+            font-weight: 700;
+        }
+        .likes{
+            display: inline-flex;
+            align-items: center;
+            gap: 4px;
+            font-size: 0.875rem;
+            color:${({theme}) => theme.color.primary};
+            margin: 0 0 4px 0;
+            font-weight: 700;
+            border: 1px solid ${({theme}) => theme.color.border};
+            border-radius: ${({theme}) => theme.borderRadius.default};
+            padding: 4px 12px;
+            position: absolute;
+            bottom: 16px;
+            right: 16px;
+
+            svg{
+                color: ${({theme}) => theme.color.primary};
+            }
+        }
+        
+    }
+`;
 export default BookItem;

8f5ba9fbe54b4b61781e33662606f8d4b5015a5e
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-16 14:20:07 +0900

    modal 기능 구현

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { useBook } from '../hooks/useBook';
@@ -12,6 +12,7 @@ import LikeButton from '../components/book/LikeButton';
 import BookReview from '@/components/book/BookReview';
 import {Tabs }from '@/components/common/Tabs';
 import { Tab } from '@/components/common/Tabs';
+import Modal from '@/components/common/Modal';
 
 
 const bookInfoList = [
@@ -55,13 +56,17 @@ const BookDetail = () => {
 
     const {bookId} = useParams();
     const {book, likeToggle, reviews, addReview} = useBook(bookId);
+    const [isImgOpen, setIsImgOpen] = useState(false);
     if(!book) return null;
     return (
         <BookDetailStyle>
             <header className='header'>
-                <div className='img'>
+                <div className='img' onClick={()=>setIsImgOpen(true)}>
                     <img src={getImgSrc(book.img)} alt={book.title}/>
                 </div>
+                <Modal isOpen={isImgOpen} onClose={()=>setIsImgOpen(false)}>
+                    <img src={getImgSrc(book.img)} alt={book.title} />
+                </Modal>
                 <div className='info'>
                     <Title size="large" color="text">
                         {book.title}
@@ -109,7 +114,9 @@ const BookDetailStyle = styled.div`
         align-items: start;
         gap: 24px;
         padding: 0 0 24px 0;
-
+        > div{
+            position: relative !important;
+        }
         .img {
             flex: 1;
             img {
